bd4f18726414148035a88f90289bfae71abedcdd
aleksandromelo <adantasm@gmail.com>
2021-03-09 18:26:21 -0300

    Create ex038.py

---
+++ ExerciciosdePython/ex038.py
@@ -0,0 +1,9 @@
+pri = int(input('Primeiro número: '))
+seg = int(input('Segundo número: '))
+
+if pri > seg:
+    print('O PRIMEIRO número é maior.')
+elif seg > pri:
+    print('O SEGUNDO número é maior')
+else:
+    print('Ambos os números são iguais.')
